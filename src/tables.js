6080a07803cdeb11831e10c7be3fa4cb902c4b42
Grindmix <71901283+Grindmix@users.noreply.github.com>
2022-02-28 00:17:03 +0300

    Some table pics

--- src/tables.js
+++ src/tables.js
@@ -1,22 +1,24 @@
-import table_1 from './img/table-1.jpg';
+import table_1 from './img/table_1.jpg';
+import table_2 from './img/table_2.jpg';
+import table_3 from './img/table_3.jpg';
 
 export default [
     {
         id: '1',
-        name: 'Table one',
+        name: 'Стол первый',
         photo: table_1,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum, leo ut blandit porttitor, nunc urna tempus tortor, quis iaculis sem massa sollicitudin risus. Suspendisse eu varius nisl. Nunc sagittis dictum scelerisque. Vestibulum commodo libero ut sagittis suscipit. Fusce massa justo, feugiat pharetra ultrices vel, convallis a tortor.'
     },
     {
         id: '2',
-        name: 'Table two',
-        photo: table_1,
+        name: 'Стол второй',
+        photo: table_2,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum, leo ut blandit porttitor, nunc urna tempus tortor, quis iaculis sem massa sollicitudin risus. Suspendisse eu varius nisl. Nunc sagittis dictum scelerisque. Vestibulum commodo libero ut sagittis suscipit. Fusce massa justo, feugiat pharetra ultrices vel, convallis a tortor.'
     },
     {
         id: '3',
-        name: 'Table three',
-        photo: table_1,
+        name: 'Стол третий',
+        photo: table_3,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum, leo ut blandit porttitor, nunc urna tempus tortor, quis iaculis sem massa sollicitudin risus. Suspendisse eu varius nisl. Nunc sagittis dictum scelerisque. Vestibulum commodo libero ut sagittis suscipit. Fusce massa justo, feugiat pharetra ultrices vel, convallis a tortor.'
     }
 ]
